d11307e92255e9fbf7d3434261051a685695e165
davelli0331 <davelli0331@hotmail.com>
2017-02-04 18:56:40 -0600

    Refactored Exercise and Muscle Groups into a joining table

--- DataSolution/DataSolution/Exercise/Tables/Exercise.sql
+++ DataSolution/DataSolution/Exercise/Tables/Exercise.sql
@@ -3,10 +3,8 @@
 	[ExerciseId]		INT IDENTITY(1,1)	NOT NULL,
 	[ExerciseName]		VARCHAR(300)	NOT NULL,
 	[Instruction]		VARCHAR(MAX)	NOT NULL,
-	[MuscleGroup]		VARCHAR(100)	NOT NULL,
 	[PushPullIndicator]	VARCHAR(4)		NOT NULL,
 	CONSTRAINT PK_Exercise_Exercise PRIMARY KEY CLUSTERED (ExerciseId),
-	CONSTRAINT FK_MuscleGroup FOREIGN KEY (MuscleGroup) REFERENCES Exercise.MuscleGroup(MuscleGroupName),
 	CONSTRAINT FK_PushPull FOREIGN KEY (PushPullIndicator) REFERENCES Exercise.PushPullIndicator(PushPull),
 	CONSTRAINT UC_Exercise_ExerciseName UNIQUE (ExerciseName)
 )
---
+++ DataSolution/DataSolution/Exercise/Tables/ExerciseMuscleGroups.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [Exercise].[ExerciseMuscleGroups]
+(
+	[ExerciseId]		INT IDENTITY(1,1)	NOT NULL,
+	[MuscleGroupName]	VARCHAR(100)		NOT NULL,
+	CONSTRAINT PK_ExerciseMuscleGroup PRIMARY KEY CLUSTERED (ExerciseId, MuscleGroupName),
+	CONSTRAINT FK_ExerciseMuscleGroup_Exercise FOREIGN KEY (ExerciseId) REFERENCES Exercise.Exercise(ExerciseId),
+	CONSTRAINT FK_ExerciseMuscleGroup_MuscleGroup FOREIGN KEY (MuscleGroupName) REFERENCES Exercise.MuscleGroup(MuscleGroupName)
+
+)
