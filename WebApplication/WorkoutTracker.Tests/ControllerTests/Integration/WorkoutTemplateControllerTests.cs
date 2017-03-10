77b30e31a24bfd21b1e5844c79958f406fd71e9f
davelli0331 <davelli0331@hotmail.com>
2017-03-10 06:00:10 -0600

    Adding integration tests

---
+++ WebApplication/WorkoutTracker.Tests/ControllerTests/Integration/WorkoutTemplateControllerTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WorkoutTracker.Core.Implementation.ActionHandlers.Concrete.WorkoutTemplateCommands;
+using WorkoutTracker.Core.Implementation.Actions.WorkoutTemplateActions;
+using WorkoutTracker.Core.Implementation.CommandDispatchers.Concrete;
+using WorkoutTracker.Persistence.DbContexts.Concrete;
+using Xunit;
+
+namespace WorkoutTracker.Tests.ControllerTests.Integration
+{
+    public class WorkoutTemplateControllerTests
+    {
+        [Fact]
+        public void Post_Succeeds()
+        {
+            var dbContext = new CommandDbContext();
+            var actionHandler = new AddWorkoutTemplateActionHandler(dbContext);
+            var dispatcher = new WorkoutTemplateCommandDispatcher(actionHandler);
+
+            //dispatcher.Dispatch(new AddWorkoutTemplateAction
+            //{
+            //    Name
+            //});
+        }
+    }
+}
