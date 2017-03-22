02589df8d8ed11356f8aa7932078adb7453c96ea
davelli0331 <davelli0331@hotmail.com>
2017-03-21 21:07:34 -0500

    Getting logic of add screen fleshed out

--- WebApplication/WorkoutTracker.Web/src/Views/WorkoutTemplate/Add.js
+++ WebApplication/WorkoutTracker.Web/src/Views/WorkoutTemplate/Add.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Immutable from 'immutable';
 import Actions from '../../Actions/WorkoutTemplate/WorkoutTemplateActions';
 import WorkoutTemplate from '../../Data/WorkoutTemplate';
+import WorkoutTemplateExercise from '../../Data/WorkoutTemplateExercise';
 
 class WorkoutTemplateAdd extends React.Component {
     constructor(props) {
@@ -32,7 +33,57 @@ class WorkoutTemplateAdd extends React.Component {
     }
 
     onExerciseNumberOfSetsChanged(exerciseId, numberOfSets) {
-        
+        let wet = this.findWorkoutTemplateExercise(exerciseId);
+        wet = wet.set('NumberOfSets', numberOfSets);
+
+        let wets = this.state.workoutTemplate.exercises.set(-1, wet);
+
+        this.setState((prevState, props) => ({
+            workoutTemplate: this.state.workoutTemplate.set('exercises', wets)
+        }));
+    }
+
+    onExerciseNumberOfRepsChanged(exerciseId, numberOfReps) {
+        let wet = this.findWorkoutTemplateExercise(exerciseId);
+        wet = wet.set('NumberOfReps', numberOfReps);
+
+        let wets = this.state.workoutTemplate.exercises.set(-1, wet);
+
+        this.setState((prevState, props) => ({
+            workoutTemplate: this.state.workoutTemplate.set('exercises', wets)
+        }));
+    }
+
+    onAddOrRemoveExerciseChecked(exerciseId, isAdd) {
+        let wets = this.state.workoutTemplate.exercises;
+        if (isAdd) {
+            let wet = new WorkoutTemplateExercise({
+                ExerciseId: exerciseId
+            });
+
+            wets = wets.push(wet);
+        }
+        else {
+            let wetIndex = this.state.workoutTemplate.exercises.findIndex((exercise) => {
+                return exercise.ExerciseId === exerciseId;
+            });
+
+            if (wetIndex > -1) {
+                wets = wets.remove(wetIndex);
+            }
+        }
+
+        this.setState((prevState, props) => ({
+            workoutTemplate: prevState.workoutTemplate.set('exercises', wets)
+        }));
+    }
+
+    findWorkoutTemplateExercise(exerciseId) {
+        let wet = this.state.workoutTemplate.exercises.find((exercise) => {
+            return exercise.ExerciseId === exerciseId;
+        });
+
+        return wet;
     }
 
     render() {
@@ -50,10 +101,10 @@ class WorkoutTemplateAdd extends React.Component {
             <ul>
                 {[...this.props.availableExercises.values()].map((exercise, index) => {
                     return (<li key={exercise.id}>
-                        <input type="checkbox" />
+                        <input type="checkbox" onChange={(e) => this.onAddOrRemoveExerciseChecked(exercise.id, e.target.checked)} />
                         {exercise.name}
-                        Sets: <input type="text" />
-                        Reps: <input type="text" />
+                        Sets: <input type="text" onChange={(e) => this.onExerciseNumberOfSetsChanged(exercise.id, e.target.value)} />
+                        Reps: <input type="text" onChange={(e) => this.onExerciseNumberOfRepsChanged(exercise.id, e.target.value)} />
                     </li>);
                 })}
             </ul>
